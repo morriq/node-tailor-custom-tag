9d4e7c8403b2373c660ff3da38cbca07aefc6546
Dawid Winiarczyk <dawid.winiarczyk@pracuj.pl>
2020-09-08 09:19:57 +0200

    bump

--- packages/recommendations/src/server.tsx
+++ packages/recommendations/src/server.tsx
@@ -5,27 +5,28 @@ import StateContext from './contexts/StateContext';
 import App from './App';
 
 const asyncSomething = (response, timeout) =>
-  new Promise((resolve) => setTimeout(() => resolve(response), timeout));
+    new Promise((resolve) => setTimeout(() => resolve(response), timeout));
 
-export default ({ recommendationsEndpoint }) => async () => {
-  const state = await Promise.all([
-    asyncSomething({ name: 'John' }, 1000),
-    asyncSomething([1, 23, 4], 100),
-  ]).then(([user, numbers]) => ({ user, numbers }));
+export default (setup) => async () => {
+    const { recommendationsEndpoint } = setup();
+    const state = await Promise.all([
+        asyncSomething({ name: 'John' }, 1000),
+        asyncSomething([1, 23, 4], 100),
+    ]).then(([user, numbers]) => ({ user, numbers }));
 
-  return renderToNodeStream(
-    <StateContext.Provider value={state}>
-      <div id='app'>
-        <App />
-      </div>
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `window.__PRELOADED_STATE__=${JSON.stringify(state).replace(
-            /</g,
-            '\\u003c'
-          )}`,
-        }}
-      />
-    </StateContext.Provider>
-  );
+    return renderToNodeStream(
+        <StateContext.Provider value={state}>
+            <div id='app'>
+                <App />
+            </div>
+            <script
+                dangerouslySetInnerHTML={{
+                    __html: `window.__PRELOADED_STATE__=${JSON.stringify(state).replace(
+                        /</g,
+                        '\\u003c'
+                    )}`,
+                }}
+            />
+        </StateContext.Provider>
+    );
 };
