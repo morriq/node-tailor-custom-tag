a5367b2cdd927e7d3e2ef0761ef1dfca240e3db9
Dawid Winiarczyk <dawid.winiarczyk@pracuj.pl>
2020-10-01 14:40:48 +0200

    bump

--- pages/offer/server/api/tailor-setup.js
+++ pages/offer/server/api/tailor-setup.js
@@ -1,15 +1,19 @@
 const { resolve } = require('path');
+const { readFileSync } = require('fs');
 
 const libraries = require('../libraries');
 
 module.exports = {
-    templatesPath: resolve(__dirname, '../../app/dist/index.html'),
+    fetchTemplate(request, parseTemplate) {
+        return parseTemplate(readFileSync(resolve(resolve(__dirname, '../../app/dist/index.html', 'utf-8'))));
+    },
     handledTags: ['library'],
     handleTag: (request, tag, options, context) => {
         if (!tag || tag.name !== 'library') {
             return '';
         }
 
+        // index.html jest nadpisywany przez webpacka bo jest zdefiniowany w webpackclient
         // @todo jesli jest devmode to webpackdevmiddleware i fetchTemplate (tutaj issue na benchmark czy nie trzeba cacheowac produkcyjnie)
         // @TODO integration test
         // @TODO https://stackoverflow.com/questions/53975046/node-js-pipe-a-stream-out-of-a-forked-child-process
@@ -20,4 +24,3 @@ module.exports = {
         return dependency.render(request);
     },
 };
-
