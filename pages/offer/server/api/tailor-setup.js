7dbe3b8ac956ede9b0cb6c2e35b272a600b94751
Dawid Winiarczyk <dawid.winiarczyk@pracuj.pl>
2020-10-01 16:21:50 +0200

    bump

--- pages/offer/server/api/tailor-setup.js
+++ pages/offer/server/api/tailor-setup.js
@@ -16,7 +16,6 @@ module.exports = {
         // index.html jest nadpisywany przez webpacka bo jest zdefiniowany w webpackclient
         // @todo jesli jest devmode to webpackdevmiddleware i fetchTemplate (tutaj issue na benchmark czy nie trzeba cacheowac produkcyjnie)
         // @TODO integration test
-        // @TODO https://stackoverflow.com/questions/53975046/node-js-pipe-a-stream-out-of-a-forked-child-process
         // @todo czesc kliencka babel runtime, externals zeby reacta nie ladowawc
 
         const dependency = libraries.get(tag.attributes.dependency)();
