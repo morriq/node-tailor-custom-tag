28499fa721de6f7cb85ba0e57b02f47c7aa69f84
morriq <dawid.winiarczyk@gmail.com>
2020-10-28 14:49:46 +0100

    fix stream

--- packages/header/src/App.tsx
+++ packages/header/src/App.tsx
@@ -3,7 +3,6 @@ import Component from './components/Component';
 import ErrorBoundary from './components/ErrorBoundary';
 
 function App(): React.ReactElement {
-  console.log(window.document);
   return <ErrorBoundary><Component /></ErrorBoundary>;
 }
 
